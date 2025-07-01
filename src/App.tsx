ac283b0db7684aca4b3bba3ac66cd2763fc2bbad
Lucas Laino <lucaslaino00@gmail.com>
2025-07-01 19:17:54 -0300

    Feat(Component): Adiciona o componente StayProductiveSection.tsx
    Adiciona o componente StayProductiveSection e a estilização
    StayProductiveSection.module.css seguindo o design fornecido (pasta design)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 import Header from './components/Header/header.tsx'
 import IntroSection from './components/IntroSection/IntroSection.tsx'
 import FyloServices from './components/FyloServices/FyloServices.tsx'
+import StayProductiveSection from './components/StayProductiveSection/StayProductiveSection.tsx'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <IntroSection />
-    <FyloServices />
+      <Header />
+      <IntroSection />
+      <FyloServices />
+      <StayProductiveSection />
     </>
   )
 }
---
+++ src/components/StayProductiveSection/StayProductiveSection.tsx
@@ -0,0 +1,21 @@
+import styles from './StayProductiveSection.module.css'
+
+function StayProductiveSection() {
+    return (
+        <section className={styles.StayProductiveSection}>
+            <img src="./src/assets/images/illustration-stay-productive.png" alt="illustration-stay-productive" />
+
+            <div className={styles.StayProductiveInfos}>
+                <h2 className={styles.StayProductiveTitle}>Stay Productive, Wherever you are</h2>
+
+                <p className={styles.AboutStayProductive}>Never let location be an issue when acessing your files. Fylo has you covered for all of your file storage needs.</p>
+
+                <p className={styles.AboutStayProductive}>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+
+                <a href="#" className={styles.BtnSeeHowFyloWorks}> See how Fylo works</a>
+            </div>
+        </section>
+    )
+}
+
+export default StayProductiveSection
