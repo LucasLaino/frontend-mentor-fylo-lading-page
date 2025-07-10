e0133598c8925f3fb086ee3200058126d62c9467
Lucas Laino <lucaslaino00@gmail.com>
2025-07-10 19:43:25 -0300

    feat(component) Adiciona o UserCommentCard

---
+++ src/components/UserCommentCard/UserCommentCard.tsx
@@ -0,0 +1,20 @@
+function UserCommentCard() {
+    return (
+        <>
+            <div>
+                <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
+
+                <div>
+                    <img src="./src/assets/images/profile-1.jpg" alt="profile-1-image"/>
+
+                    <div>
+                        <h4>Satish Patel</h4>
+                        <p>Founder & CEO, Huddle</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UserCommentCard
