904e579ba4179f5b922771a700a28b7612774cc9
Lucas Laino <lucaslaino00@gmail.com>
2025-07-10 20:12:52 -0300

    feat(component): Adiciona o CommentSection.module.css

--- src/components/CommentsSection/CommentsSection.tsx
+++ src/components/CommentsSection/CommentsSection.tsx
@@ -1,4 +1,5 @@
 import styles from './CommentsSection.module.css'
+import UserCommentCard from '../UserCommentCard/UserCommentCard'
 
 // Criar um componente separado para listar os comentários. O código repete muito
 function CommentsSection() {
@@ -7,44 +8,7 @@ function CommentsSection() {
             <img src="./src/assets/images/bg-quotes.png" alt="bg-quotes" className={styles.bgQuotes} />
 
             <section className={styles.CommentsSection}>
-                <div className={styles.CommentCard}>
-                    <p className={styles.UserMessage}>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
-
-                    <div className={styles.UserProfile}>
-                        <img src="./src/assets/images/profile-1.jpg" alt="profile-1-image" className={styles.UserProfileImg} />
-
-                        <div className={styles.UserInfos}>
-                            <h4 className={styles.UserName}>Satish Patel</h4>
-                            <p className={styles.UserOcupation}>Founder & CEO, Huddle</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={styles.CommentCard}>
-                    <p className={styles.UserMessage}>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
-
-                    <div className={styles.UserProfile}>
-                        <img src="./src/assets/images/profile-2.jpg" alt="profile-2-image" className={styles.UserProfileImg} />
-
-                        <div className={styles.UserInfos}>
-                            <h4 className={styles.UserName}>Satish Patel</h4>
-                            <p className={styles.UserOcupation}>Founder & CEO, Huddle</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={styles.CommentCard}>
-                    <p className={styles.UserMessage}>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
-
-                    <div className={styles.UserProfile}>
-                        <img src="./src/assets/images/profile-3.jpg" alt="profile-3-image" className={styles.UserProfileImg} />
-
-                        <div className={styles.UserInfos}>
-                            <h4 className={styles.UserName}>Satish Patel</h4>
-                            <p className={styles.UserOcupation}>Founder & CEO, Huddle</p>
-                        </div>
-                    </div>
-                </div>
+                <UserCommentCard />
             </section>
         </section>
     )
--- src/components/UserCommentCard/UserCommentCard.tsx
+++ src/components/UserCommentCard/UserCommentCard.tsx
@@ -1,15 +1,17 @@
+import styles from './UserCommentCard.module.css'
+
 function UserCommentCard() {
     return (
         <>
-            <div>
-                <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
+            <div className={styles.CommentCard}>
+                <p className={styles.UserComment}>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
 
-                <div>
-                    <img src="./src/assets/images/profile-1.jpg" alt="profile-1-image"/>
+                <div className={styles.UserInfos}>
+                    <img src="./src/assets/images/profile-1.jpg" alt="profile-1-image" className={styles.UserProfileImg}/>
 
                     <div>
                         <h4>Satish Patel</h4>
-                        <p>Founder & CEO, Huddle</p>
+                        <p className={styles.UserOcupation}>Founder & CEO, Huddle</p>
                     </div>
                 </div>
             </div>
