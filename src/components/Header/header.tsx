4a29cddf410c4a56840c37cf5275f46704bb7acb
Lucas Laino <lucaslaino00@gmail.com>
2025-07-14 19:09:59 -0300

    perf: Adiciona Media Queries ao header.tsx

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 function Header() {
   return (
     <header className={styles.header}>
-      <img src="./src/assets/images/logo.svg" alt="logo-fylo" />
+      <img src="./src/assets/images/logo.svg" alt="logo-fylo" className={styles.logo}/>
       <nav>
         <ul className={styles.nav}>
           <li><a className={styles.navOptions} href="#">Features</a></li>
